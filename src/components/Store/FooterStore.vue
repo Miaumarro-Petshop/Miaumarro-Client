0d58c4f06d15c1ea0df7ba3aa51bc20a3d601507
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-22 23:41:18 -0400

    created basic vue app structure

---
+++ src/components/Store/FooterStore.vue
@@ -0,0 +1,68 @@
+<template>
+  <footer>
+    <nav class="navbar ts14r" id="nav-footer">
+      <li id="nav-about">
+        <h1 class="ts18b">Sobre</h1>
+        <ul>
+          <a href="">Quem somos</a>
+        </ul>
+        <ul>
+          <a href="">Política de privacidade</a>
+        </ul>
+      </li>
+      <li id="nav-help">
+        <h1 class="ts18b">Ajuda</h1>
+        <ul>
+          <a href="">Dúvidas frequentes</a>
+        </ul>
+        <ul>
+          <a href="">Trocas e devoluções</a>
+        </ul>
+        <ul>
+          <a href="">Fale conosco</a>
+        </ul>
+      </li>
+      <li id="nav-oppening-hours">
+        <h1 class="ts18b">Atendimento</h1>
+        <ul>
+          <p>Seg a Sex: 09h às 22h</p>
+        </ul>
+        <ul>
+          <p>Sáb: 10h às 20h</p>
+        </ul>
+        <ul>
+          <p>Dom: 10h às 16h</p>
+        </ul>
+      </li>
+      <li id="nav-social-networks">
+        <h1 class="ts18b">Redes Sociais</h1>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <a href=""
+                ><img src="../../assets/img/icon/instagram.svg" class="icon-32"
+              /></a>
+            </div>
+            <div class="col">
+              <a href=""
+                ><img src="../../assets/img/icon/whatsapp.svg" class="icon-32"
+              /></a>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+              <a href=""
+                ><img src="../../assets/img/icon/facebook.svg" class="icon-32"
+              /></a>
+            </div>
+            <div class="col">
+              <a href=""
+                ><img src="../../assets/img/icon/tiktok.svg" class="icon-32"
+              /></a>
+            </div>
+          </div>
+        </div>
+      </li>
+    </nav>
+  </footer>
+</template>
+<script></script>
