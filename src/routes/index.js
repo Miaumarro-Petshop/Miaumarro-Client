bbc70a98c2d189e16761a779e15546acec5f9376
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-23 19:14:30 -0400

    Added routes do store pages (Cart, Wishlist)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import StoreHome from "../views/StoreHome.vue";
-import ProductSearch from "../views/Store/ProductSearch.vue";
-import ProductDetail from "../views/Store/ProductDetail.vue";
+import ProductSearch from "../views/Store/Product/ProductSearch.vue";
+import ProductDetail from "../views/Store/Product/ProductDetail.vue";
+import WishListView from "../views/Store/Wishlist/WishListView.vue";
+import CartList from "../views/Store/Cart/CartList.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,16 @@ const router = createRouter({
       path: "/produto",
       name: "product-detail",
       component: ProductDetail,
+    },
+    {
+      path: "/wishlist",
+      name: "wishlist",
+      component: WishListView,
+    },
+    {
+      path: "/carrinho",
+      name: "cart",
+      component: CartList,
     },
   ],
 });
