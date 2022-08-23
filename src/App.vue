f71ad965ea6434be9f7377e51a13ea57eedfe5f5
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-23 00:18:56 -0400

    Added MainStore component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderStore from "./components/Store/HeaderStore.vue";
 import FooterStore from "./components/Store/FooterStore.vue";
+import MainPage from "./components/Store/MainPage.vue";
 </script>
 
 <template>
-  <HeaderStore> </HeaderStore>
-  <FooterStore> </FooterStore>
+  <HeaderStore></HeaderStore>
+  <MainPage></MainPage>
+  <FooterStore></FooterStore>
 </template>
