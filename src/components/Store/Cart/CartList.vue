76c78e726c1ca294096b0b45fc78582196db5321
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-24 16:42:53 -0400

    Fixed getProducts method

--- src/components/Store/Cart/CartList.vue
+++ src/components/Store/Cart/CartList.vue
@@ -1,12 +1,12 @@
 <template>
-  <button @click="getProducts">Show Products</button>
-  <hr />
-  <div v-for="p in products" :key="p.id">
-    <h2>{{ p.id }} - {{ p.name }}</h2>
-    <p>{{ p.description }}</p>
-  </div>
+  <main>
+    <div v-for="p in products" :key="p.id">
+      <h2>{{ p.id }} - {{ p.name }}</h2>
+      <p>Descrição: {{ p.description }}</p>
+      <p>Marca: {{ p.brand }}</p>
+    </div>
+  </main>
 </template>
-
 <script>
 export default {
   data() {
@@ -18,8 +18,9 @@ export default {
     async getProducts() {
       var resposta = await fetch("https://localhost:7016/api/v1/Product");
       var json = await resposta.json();
-      this.products = JSON.stringify(json.response);
-      window.alert(JSON.stringify(json.response));
+      this.products = json.response;
+      //this.products = JSON.stringify(json.response);
+      //window.alert(this.products);
     },
   },
   beforeMount() {
