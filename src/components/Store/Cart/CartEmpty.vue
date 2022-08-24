ef0cf4dbbf4a88d0a455914bc0fcf784c52dbaec
Maíra Heloísa Silva Oliveira <heloisa3031@gmail.com>
2022-08-24 20:50:00 -0300

    Cart components creation

--- src/components/Store/Cart/CartEmpty.vue
+++ src/components/Store/Cart/CartEmpty.vue
@@ -1,5 +1,16 @@
 <template>
 <main>
-</main>
+        <section class="customer-empty-container">
+            <div class="card card-empty col col-lg-10">
+                <img src="../../img/icon/cart-fill.svg" class="icon-64 icon-purple" alt="Cart icon">
+                <h1 class="ts18b ts-purple">Seu carrinho está vazio no momento</h1>
+                <h2 class="ts14r col-lg-6">Para continuar comprando, navegue pelas categorias e faça uma busca por produtos.</h2>
+            </div>
+
+            <a href="../index.html" class="row">
+                <button class="btn btn-purple btn-48 ts18b" type="submit">Continuar navegando</button>
+            </a>
+        </section>
+    </main>
 </template>
 <script></script>
