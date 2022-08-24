4f94aaca168f2af17eb877ebedae8eb21057f0bc
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-24 15:43:25 -0400

    API consuming test

--- src/components/Store/Cart/CartList.vue
+++ src/components/Store/Cart/CartList.vue
@@ -1,5 +1,29 @@
 <template>
-<main>
-</main>
+  <button @click="getProducts">Show Products</button>
+  <hr />
+  <div v-for="p in products" :key="p.id">
+    <h2>{{ p.id }} - {{ p.name }}</h2>
+    <p>{{ p.description }}</p>
+  </div>
 </template>
-<script></script>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    async getProducts() {
+      var resposta = await fetch("https://localhost:7016/api/v1/Product");
+      var json = await resposta.json();
+      this.products = JSON.stringify(json.response);
+      window.alert(JSON.stringify(json.response));
+    },
+  },
+  beforeMount() {
+    this.getProducts();
+  },
+};
+</script>
