d36279cab8fa47fd9da46299c25ce29fa750f2c9
Maíra Heloísa Silva Oliveira <heloisa3031@gmail.com>
2022-08-24 22:12:35 -0300

    Address components creation

--- src/components/User/Address/AddressEmpty.vue
+++ src/components/User/Address/AddressEmpty.vue
@@ -1,4 +1,25 @@
 <template>
-  <main></main>
+  <main>
+        <article class="account">
+        <section>
+            <h1 class="ts40 ts-purple">Meus endereços</h1>
+        </section>
+        <section>
+            <p class="ts12r ts-green">Mantenha seus endereços sempre atualizados e identificados para facilitar sua compra.</p>
+        </section>
+        <section class="customer-empty-container">
+            <div class="card card-empty col col-lg-10">
+                <img src="../../../assets/img/icon/geo-alt-fill.svg" class="icon-64" alt="Map icon">
+                <h1 class="ts18b ts-purple">Você não tem endereços cadastrados</h1>
+                <h2 class="ts14r col-lg-6">Você ainda não adicionou nenhum endereço.
+                    Adicione um novo endereço no botão abaixo.</h2>
+            </div>
+
+            <a href="#" class="row">
+                <button class="btn btn-purple btn-48 ts18b" type="submit">Adicionar endereço</button>
+            </a>
+        </section>
+    </article>
+    </main>
 </template>
 <script></script>
--- src/components/User/HeaderUser.vue
+++ src/components/User/HeaderUser.vue
@@ -1,5 +1,88 @@
 <template>
-  <h1>Teste</h1>
+ <header>
+        <div id="nav-main-lg">
+            <nav class="navbar primary-menu">
+                <a href="../index.html" class="navbar-brand">
+                    <img src="../../assets/img/miaumarro/Logo_dynapuff.png" height="40">
+                </a>
+
+                <form id="search-bar" class="form-inline container-fluid">
+                    <input class="ts12r form-black" type="search" id="searchedTerm" placeholder="O que você está procurando?" aria-label="Search"/>
+                    <div class="form-button">
+                        <button class="btn btn-grey btn-icon" type="submit">
+                            <img class="icon-24" src="../../assets/img/icon/search.svg">
+                        </button>
+                    </div>
+                </form>
+
+                <li class="customer-options">
+                    <ul class="customer-options-items">
+                        <a href="#" class="login-options">
+                            <img class="icon-32" src="../../assets/img/icon/person-circle.svg">
+                            <div class="hidden-xs login-options-text">
+                                <p class="ts18r">Bem vindo(a)</p>
+                                <p class="ts12r">Entre ou Cadastre-se</p>
+                            </div>
+                        </a>
+
+                    </ul>
+                    <ul class=" customer-options-items">
+                        <a href="wishlist.html">
+                            <img class="icon-32" src="../../assets/img/icon/heart-fill.svg">
+                        </a>
+                    </ul>
+                    <ul class="customer-options-items">
+                        <a href="cart.html">
+                            <img class="icon-32" src="../../assets/img/icon/cart-fill.svg">
+                        </a>
+                    </ul>
+                </li>
+            </nav>
+
+            <nav class="navbar secondary-menu my-data main ts18b">
+                <li class="my-data-options">
+                    <ul class="my-data-options-items">
+                        <a href="">Meus dados</a>
+                    </ul>
+                    <ul class="my-data-options-items">
+                        <a href="">Meus endereços</a>
+                    </ul>
+                    <ul class="my-data-options-items">
+                        <a href="">Meus bichinhos</a>
+                    </ul>
+                    <ul class="my-data-options-items">
+                        <a href="">Meus pedidos</a>
+                    </ul>
+                    <ul class="my-data-options-items">
+                        <a href="">Meus agendamentos</a>
+                    </ul>
+                </li>
+            </nav>
+        </div>
+        <div id="nav-main-md">
+            <div class="navbar container-fluid">
+                <a id="nav-main-md-menu" href="#">
+                    <img class="icon-32" src="../../assets/img/icon/list.svg">
+                </a>
+                <a id="nav-main-md-brand" href="index.html">
+                    <img src="../../assets/img/miaumarro/Logo_dynapuff.png" height="32">
+                </a>
+                <a id="nav-main-md-cart" href="cart.html">
+                    <img class="icon-32" src="../../assets/img/icon/cart-fill.svg">
+                </a>
+            </div>
+            <div class="navbar container-fluid">
+                <form id="search-bar" class="form-inline container-fluid">
+                    <input class="ts12r form-black" type="search" id="searchedTerm" placeholder="O que você está procurando?" aria-label="Search"/>
+                    <div class="form-button">
+                        <button class="btn btn-grey btn-icon" type="submit">
+                            <img class="icon-24" src="../../assets/img/icon/search.svg">
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </header>
 </template>
 <script>
 export default {
