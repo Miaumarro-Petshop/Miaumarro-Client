967856e9c71813ef1fdf4d8f59990c2f57a9e10e
Maíra Heloísa Silva Oliveira <heloisa3031@gmail.com>
2022-08-24 22:26:23 -0300

    Appointment components creation

--- src/components/User/Appointment/AppointmentEmpty.vue
+++ src/components/User/Appointment/AppointmentEmpty.vue
@@ -1,4 +1,36 @@
 <template>
-  <main></main>
+  <main>
+        <article class="account">
+        <section>
+            <h1 class="ts40 ts-purple">Meus agendamentos</h1>
+        </section>
+        <section>
+            <p class="ts12r ts-green">Acompanhe aqui os seus serviços agendados!</p>
+        </section>
+        <section class="customer-empty-container">
+            <div class="card card-empty col col-lg-10">
+                <img src="../../../assets/img/icon/calendar-event-fill.svg" class="icon-64 icon-purple" alt="Calendar icon">
+                <h1 class="ts18b ts-purple">Você não tem agendamentos futuros</h1>
+                <h2 class="ts14r col-lg-6">Seus bichinhos não têm nenhum serviço agendado.
+                    Agende um serviço no botão abaixo ou confira seu histórico de agendamentos.</h2>
+            </div>
+
+            <div>
+                <nav>
+                    <a class="ts14b link-green">
+                        <p>
+                            histórico de agendamentos
+                        </p>
+                    </a>
+                </nav>
+            </div>
+            <div class="col-md-8 col-lg-8 cards-list" id="center-btn">
+                <a href="">
+                    <button class="btn btn-purple ts18b" type="submit">Novo agendamento</button>
+                </a>
+            </div>
+        </section>
+    </article>
+    </main>
 </template>
 <script></script>
