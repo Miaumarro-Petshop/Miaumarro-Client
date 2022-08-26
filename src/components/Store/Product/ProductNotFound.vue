9c65dc596d49aa729e1f0fdda95d653ddeddd8ca
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-25 22:38:49 -0400

    Error in product tags query

---
+++ src/components/Store/Product/ProductNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+<main>
+        <section class="customer-empty-container">
+            <div class="card card-empty col col-lg-10">
+                <img src="../../../assets/img/miaumarro/Logo_icon.svg" class="icon-64 icon-purple" alt="Cart icon">
+                <h1 class="ts18b ts-purple">Busca não encontrada :(</h1>
+                <h2 class="ts14r col-lg-6">Não encontramos produtos com essas especificações. Tente realizar uma nova busca com outros termos ou navegue pelas categorias.</h2>
+            </div>
+
+            <a href="../index.html" class="row">
+                <button class="btn btn-purple btn-48 ts18b" type="submit">Continuar navegando</button>
+            </a>
+        </section>
+    </main>
+</template>
+<script></script>
--- src/components/Store/Product/ProductSearch.vue
+++ src/components/Store/Product/ProductSearch.vue
@@ -27,7 +27,7 @@
             data-bs-parent="#accordionExample"
           >
             <div class="accordion-body">
-              <form class="form-product-filter">
+              <form class="form-product-filter" method="get">
                 <div class="product-filter-options">
                   <div class="product-filter-text col col-lg-4">
                     <div class="row">
@@ -35,6 +35,7 @@
                     </div>
                     <div class="row">
                       <input
+                        v-model="SearchedTerm"
                         class="ts12r form-black"
                         type="text"
                         id="searchedTerm"
@@ -47,6 +48,7 @@
                     </div>
                     <div class="row">
                       <input
+                        v-model="Brand"
                         class="ts12r form-black"
                         type="text"
                         id="brand"
@@ -58,7 +60,12 @@
                       <label class="ts14b">Preço</label>
                     </div>
                     <div class="row">
+                      <label class="ts12r col-6">Mínimo</label>
+                      <label class="ts12r col-6">Máximo</label>
+                    </div>
+                    <div class="row">
                       <input
+                        v-model="MinPrice"
                         class="ts12r form-black col-6"
                         type="text"
                         id="MinPrice"
@@ -66,6 +73,7 @@
                         aria-label="Preço Mínimo"
                       />
                       <input
+                        v-model="MaxPrice"
                         class="ts12r form-black col-6"
                         type="text"
                         id="MaxPrice"
@@ -80,10 +88,11 @@
                       <div class="product-tags">
                         <div class="checkbox-item">
                           <input
+                            v-bind="SmallPet"
                             type="checkbox"
                             id="SmallPet"
-                            name="SmallPet"
-                            value="0"
+                            name="product-tags"
+                            value="1"
                           />
                           <label for="SmallPet" class="ts14b"
                             >Pequeno porte</label
@@ -91,10 +100,11 @@
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="MediumPet"
                             type="checkbox"
                             id="MediumPet"
-                            name="MediumPet"
-                            value="1"
+                            name="product-tags"
+                            value="2"
                           />
                           <label for="MediumPet" class="ts14b"
                             >Médio porte</label
@@ -102,10 +112,11 @@
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="LargePet"
                             type="checkbox"
                             id="LargePet"
-                            name="LargePet"
-                            value="2"
+                            name="product-tags"
+                            value="4"
                           />
                           <label for="LargePet" class="ts14b"
                             >Grande porte</label
@@ -113,19 +124,21 @@
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Clothing"
                             type="checkbox"
                             id="Clothing"
-                            name="Clothing"
-                            value="3"
+                            name="product-tags"
+                            value="8"
                           />
                           <label for="Clothing" class="ts14b">Roupas</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Medicine"
                             type="checkbox"
                             id="Medicine"
-                            name="Medicine"
-                            value="4"
+                            name="product-tags"
+                            value="16"
                           />
                           <label for="Medicine" class="ts14b"
                             >Medicamentos</label
@@ -133,37 +146,41 @@
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Furniture"
                             type="checkbox"
                             id="Furniture"
-                            name="Furniture"
-                            value="5"
+                            name="product-tags"
+                            value="32"
                           />
                           <label for="Furniture" class="ts14b">Mobília</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Food"
                             type="checkbox"
                             id="Food"
-                            name="Food"
-                            value="6"
+                            name="product-tags"
+                            value="64"
                           />
                           <label for="Food" class="ts14b">Alimentação</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Toy"
                             type="checkbox"
                             id="Toy"
-                            name="Toy"
-                            value="7"
+                            name="product-tags"
+                            value="128"
                           />
                           <label for="Toy" class="ts14b">Brinquedos</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Accessory"
                             type="checkbox"
                             id="Accessory"
-                            name="Accessory"
-                            value="8"
+                            name="product-tags"
+                            value="256"
                           />
                           <label for="Accessory" class="ts14b"
                             >Acessórios</label
@@ -171,46 +188,51 @@
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Dog"
                             type="checkbox"
                             id="Dog"
-                            name="Dog"
-                            value="9"
+                            name="product-tags"
+                            value="512"
                           />
                           <label for="Dog" class="ts14b">Cachorros</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Cat"
                             type="checkbox"
                             id="Cat"
-                            name="Cat"
-                            value="10"
+                            name="product-tags"
+                            value="1024"
                           />
                           <label for="Cat" class="ts14b">Gatos</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Bird"
                             type="checkbox"
                             id="Bird"
-                            name="Bird"
-                            value="11"
+                            name="product-tags"
+                            value="2048"
                           />
                           <label for="Bird" class="ts14b">Pássaros</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="Fish"
                             type="checkbox"
                             id="Fish"
-                            name="Fish"
-                            value="12"
+                            name="product-tags"
+                            value="4096"
                           />
                           <label for="Fish" class="ts14b">Peixes</label>
                         </div>
                         <div class="checkbox-item">
                           <input
+                            v-bind="OtherPet"
                             type="checkbox"
                             id="OtherPet"
-                            name="OtherPet"
-                            value="13"
+                            name="product-tags"
+                            value="8192"
                           />
                           <label for="OtherPet" class="ts14b"
                             >Outros pets</label
@@ -222,9 +244,10 @@
                     <div class="row product-discount">
                       <div class="checkbox-item">
                         <input
+                          v-model="ActiveDiscount"
                           type="checkbox"
                           id="ActiveDiscount"
-                          name="activeDiscount"
+                          name="ActiveDiscount"
                         />
                         <label for="ActiveDiscount" class="ts14b"
                           >Somente produtos com desconto</label
@@ -237,15 +260,16 @@
                         >Ordernar por:</label
                       >
                       <select
+                        v-model="SortParameter"
                         name="sort-order"
                         id="sort-order"
                         form="sortform"
                         class="col-8 ts14b"
                       >
-                        <option value="PriceAsc" selected>Menor preço</option>
-                        <option value="PriceDes">Maior preço</option>
-                        <option value="DiscountAsc">Menor desconto</option>
-                        <option value="DiscountDesc">Maior Desconto</option>
+                        <option value="0">Menor preço</option>
+                        <option value="1">Maior preço</option>
+                        <option value="2">Menor desconto</option>
+                        <option value="3">Maior Desconto</option>
                       </select>
                     </div>
                   </div>
@@ -253,6 +277,7 @@
 
                 <div class="product-filter-button">
                   <button
+                    v-on:click="filterProducts()"
                     class="btn btn-purple ts18b col col-md-4 col-lg-4"
                     type="submit"
                   >
@@ -266,82 +291,92 @@
       </div>
     </section>
 
-    <section id="product-result">
-      <div class="result-count">
-        <p class="ts18b">XXX resultados encontrados</p>
-      </div>
-      <div class="product-grid row gx-2 gy-1">
-        <div
-          v-for="p in products"
-          :key="p.id"
-          class="col col-sm-3 col-md-3 col-lg-3"
-        >
-          <a v-on:click="showProductDetail(p.id)">
-            <div class="card card-product-search">
-              <div class="card-img">
-                <img
-                  class="card-img-top"
-                  src="src/assets/img/miaumarro/logo_icon.svg"
-                  alt="Product Image"
-                />
-              </div>
-              <div class="card-body">
-                <div>
-                  <h1 class="card-title ts14b">{{ p.brand }}</h1>
-                  <h2 class="card-text ts14r ts-green">
-                    {{ p.name }}
-                  </h2>
-                </div>
-                <div>
-                  <p class="card-text ts28b">R$ {{ p.price }}</p>
-                  <button class="btn btn-green btn-buy ts24r" type="submit">
-                    Comprar
-                  </button>
-                </div>
-              </div>
-            </div>
-          </a>
+    <div v-if="resultAmount">
+      <section id="product-result">
+        <div class="result-count">
+          <p class="ts18b">{{ resultAmount }} resultados encontrados</p>
         </div>
-      </div>
+        <div class="product-grid row gx-2 gy-1">
+          <div
+            v-for="p in products"
+            :key="p.id"
+            class="col col-sm-3 col-md-3 col-lg-3"
+          >
+            <a v-on:click="showProductDetail(p.id)">
+              <div class="card card-product-search">
+                <div class="card-img">
+                  <img
+                    class="card-img-top"
+                    src="src/assets/img/miaumarro/logo_icon.svg"
+                    alt="Product Image"
+                  />
+                </div>
+                <div class="card-body">
+                  <div>
+                    <h1 class="card-title ts14b">{{ p.brand }}</h1>
+                    <h2 class="card-text ts14r ts-green">
+                      {{ p.name }}
+                    </h2>
+                  </div>
+                  <div>
+                    <p class="card-text ts28b">
+                      R$
+                      {{ Math.round(p.price * (1 - p.discount) * 100) / 100 }}
+                    </p>
+                    <button class="btn btn-green btn-buy ts24r" type="submit">
+                      Comprar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
 
-      <nav aria-label="Page navigation">
-        <ul class="pagination justify-content-center">
-          <li v-if="hasPreviousPage" class="page-item">
-            <a class="page-link ts-purple ts18b" href="#" tabindex="-1">
-              <span aria-hidden="true" class="ts24r">&lt;</span>
-              <span class="sr-only">Anterior</span>
-            </a>
-          </li>
-          <li v-else class="page-item disabled">
-            <a class="page-link ts-purple ts18b" href="#" tabindex="-1">
-              <span aria-hidden="true" class="ts24r">&lt;</span>
-              <span class="sr-only">Anterior</span>
-            </a>
-          </li>
-          <li class="page-item">
-            <p class="ts24b ts-purple" href="#">{{ currentPage }}</p>
-          </li>
-          <li class="page-item">
-            <p class="ts24r" href="#">de {{ totalPages }}</p>
-          </li>
-          <li v-if="hasNextPage" class="page-item">
-            <a class="page-link ts18b ts-purple" href="#">
-              <span class="sr-only">Próxima</span>
-              <span aria-hidden="true" class="ts24r">&gt;</span>
-            </a>
-          </li>
-          <li v-else class="page-item disabled">
-            <a class="page-link ts18b ts-purple" href="#">
-              <span class="sr-only">Próxima</span>
-              <span aria-hidden="true" class="ts24r">&gt;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </section>
+        <nav aria-label="Page navigation">
+          <ul class="pagination justify-content-center">
+            <li v-if="hasPreviousPage" class="page-item">
+              <a class="page-link ts-purple ts18b" href="#" tabindex="-1">
+                <span aria-hidden="true" class="ts24r">&lt;</span>
+                <span class="sr-only">Anterior</span>
+              </a>
+            </li>
+            <li v-else class="page-item disabled">
+              <a class="page-link ts-purple ts18b" href="#" tabindex="-1">
+                <span aria-hidden="true" class="ts24r">&lt;</span>
+                <span class="sr-only">Anterior</span>
+              </a>
+            </li>
+            <li class="page-item">
+              <p class="ts24b ts-purple" href="#">{{ currentPage }}</p>
+            </li>
+            <li class="page-item">
+              <p class="ts24r" href="#">de {{ totalPages }}</p>
+            </li>
+            <li v-if="hasNextPage" class="page-item">
+              <a class="page-link ts18b ts-purple" href="#">
+                <span class="sr-only">Próxima</span>
+                <span aria-hidden="true" class="ts24r">&gt;</span>
+              </a>
+            </li>
+            <li v-else class="page-item disabled">
+              <a class="page-link ts18b ts-purple" href="#">
+                <span class="sr-only">Próxima</span>
+                <span aria-hidden="true" class="ts24r">&gt;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </div>
+    <div v-else>
+      <ProductNotFound></ProductNotFound>
+    </div>
   </main>
 </template>
 <script>
+import QueryBuilder from "../../../helpers/QueryBuilder";
+import ProductNotFound from "./ProductNotFound.vue";
 export default {
   data() {
     return {
@@ -351,11 +386,44 @@ export default {
       totalPages: 0,
       hasPreviousPage: false,
       hasNextPage: false,
+      filter: this.$route.params.filter,
+
+      SearchedTerm: null,
+      Brand: null,
+      MinPrice: 0,
+      MaxPrice: 0,
+      ActiveDiscount: false,
+      tags: 0,
+      /*
+      SortParameter: 0,
+      PageNumber: 0,
+      SmallPet: 0,
+      MediumPet: 0,
+      LargePet: 0,
+      Clothing: 0,
+      Medicine: 0,
+      Furniture: 0,
+      Food: 0,
+      Toy: 0,
+      Accessory: 0,
+      Dog: 0,
+      Cat: 0,
+      Bird: 0,
+      Fish: 0,
+      OtherPet: 0,
+      */
     };
   },
   methods: {
     async getProducts() {
-      var resposta = await fetch(`https://localhost:7016/api/v1/Product`);
+      alert(`Entrou em getProducts. filters=${this.filter}`);
+      var resposta = await fetch(
+        `https://localhost:7016/api/v1/Product?${this.filter}&PageNumber=${this.currentPage}`
+      );
+
+      alert(
+        `Link da requisição: https://localhost:7016/api/v1/Product?${this.filter}&PageNumber=${this.currentPage}`
+      );
       var json = await resposta.json();
       this.products = json.response;
       this.currentPage = json.pageNumber + 1;
@@ -367,9 +435,30 @@ export default {
     showProductDetail(productId) {
       this.$router.push(`/produto/${productId}`);
     },
+    filterProducts() {
+      alert("Entrou em filter");
+      const query = new QueryBuilder().addProperty(
+        "SortParameter",
+        this.SortParameter
+      );
+      if (this.SearchedTerm != null)
+        query.addProperty("SearchedTerm", this.SearchedTerm);
+      if (this.Brand != null) query.addProperty("Brand", this.Brand);
+      if (this.MinPrice != 0) query.addProperty("MinPrice", this.MinPrice);
+      if (this.MaxPrice != 0) query.addProperty("MaxPrice", this.MaxPrice);
+      if (this.ActiveDiscount)
+        query.addProperty("ActiveDiscount", this.ActiveDiscount);
+      //if (this.tags != 0) query.addProperty("Tags", this.tags);
+      var queryParameters = query.build();
+      alert(`queryparameters que vão para a rota = ${queryParameters}`);
+      this.$router.push(`/pesquisa/${queryParameters}`);
+    },
   },
   beforeMount() {
     this.getProducts();
+  },
+  components: {
+    ProductNotFound: ProductNotFound,
   },
 };
 </script>
