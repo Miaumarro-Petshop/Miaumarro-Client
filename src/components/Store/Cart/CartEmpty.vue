980ca7e067911a68fd6a8f8a85f214923734004d
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-08-27 19:00:30 -0400

    Added function to delete cart in localStorage

--- src/components/Store/Cart/CartEmpty.vue
+++ src/components/Store/Cart/CartEmpty.vue
@@ -1,16 +1,27 @@
 <template>
-<main>
-        <section class="customer-empty-container">
-            <div class="card card-empty col col-lg-10">
-                <img src="../../../assets/img/icon/cart-fill.svg" class="icon-64 icon-purple" alt="Cart icon">
-                <h1 class="ts18b ts-purple">Seu carrinho está vazio no momento</h1>
-                <h2 class="ts14r col-lg-6">Para continuar comprando, navegue pelas categorias e faça uma busca por produtos.</h2>
-            </div>
+  <main>
+    <section class="customer-empty-container">
+      <div class="card card-empty col col-lg-10">
+        <img
+          src="../../../assets/img/icon/cart-fill.svg"
+          class="icon-64 icon-purple"
+          alt="Cart icon"
+        />
+        <h1 class="ts18b ts-purple">Seu carrinho está vazio no momento</h1>
+        <h2 class="ts14r col-lg-6">
+          Para continuar comprando, navegue pelas categorias e faça uma busca
+          por produtos.
+        </h2>
+      </div>
 
-            <a href="../index.html" class="row">
-                <button class="btn btn-purple btn-48 ts18b" type="submit">Continuar navegando</button>
-            </a>
-        </section>
-    </main>
+      <RouterLink to="/">
+        <a class="row">
+          <button class="btn btn-purple btn-48 ts18b" type="submit">
+            Continuar navegando
+          </button>
+        </a>
+      </RouterLink>
+    </section>
+  </main>
 </template>
 <script></script>
--- src/components/Store/Cart/CartList.vue
+++ src/components/Store/Cart/CartList.vue
@@ -4,124 +4,75 @@
       <div v-if="resultAmount">
         <div class="cart-title">
           <h1 class="ts40 ts-purple">Carrinho</h1>
-          <p class="ts18b">Produtos</p>
         </div>
         <div class="cart-products">
           <div class="result-count">
-            <p class="ts18b">XXX produtos encontrados</p>
+            <p class="ts18b">{{ resultAmount }} produtos encontrados</p>
           </div>
 
           <div class="cart-info">
             <div class="purchase-required-info">
               <li class="cards-list space">
-                <ul>
-                  <a href="">
-                    <div class="card card-product-cart">
-                      <div class="card-img-cl col col-lg-2">
-                        <img
-                          src="../../../assets/img/miaumarro/logo_icon.svg"
-                          alt="Product Image"
-                        />
-                      </div>
-                      <div class="card-body col col-lg-10">
-                        <div class="card-info">
-                          <div class="card-title">
-                            <h1 class="ts24b">
-                              Nome Completo do Produto Nome Completo do Produto
-                            </h1>
-                            <h2 class="ts14b ts-purple">
-                              CÓDIGO DO PRODUTO: XXXXXX
-                            </h2>
-                          </div>
-                          <div class="card-text-cart">
-                            <div class="product-amount">
-                              <div class="counter">
-                                <button
-                                  class="btn btn-purple ts24r"
-                                  onclick="this.blur();"
-                                >
-                                  -
-                                </button>
-                                <div class="counter-display ts24r ts-black">
-                                  01
-                                </div>
-                                <button
-                                  class="btn btn-purple ts24r"
-                                  onclick="this.blur();"
-                                >
-                                  +
-                                </button>
-                              </div>
+                <div
+                  v-for="p in products"
+                  :key="p.id"
+                  class="col col-sm-3 col-md-3 col-lg-3"
+                >
+                  <a v-on:click="showProductDetail(p.id)">
+                    <ul>
+                      <div class="card card-product-cart">
+                        <div class="card-img-cl col col-lg-2">
+                          <img
+                            src="../../../assets/img/miaumarro/logo_icon.svg"
+                            alt="Product Image"
+                          />
+                        </div>
+                        <div class="card-body col col-lg-10">
+                          <div class="card-info">
+                            <div class="card-title">
+                              <h1 class="ts24b">
+                                {{ p.name }}
+                              </h1>
+                              <h2 class="ts14b ts-purple">
+                                CÓDIGO DO PRODUTO: {{ p.id }}
+                              </h2>
                             </div>
-                            <h3 class="card-text ts28b">R$ 100,00</h3>
-                          </div>
-                        </div>
-                        <div class="card-icon">
-                          <a href="">
-                            <img
-                              src="../../../assets/img/icon/trash-fill-purple.svg"
-                              class="icon-32"
-                            />
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </ul>
-                <ul>
-                  <a href="">
-                    <div class="card card-product-cart">
-                      <div class="card-img-cl col col-lg-2">
-                        <img
-                          src="../../../assets/img/miaumarro/logo_icon.svg"
-                          alt="Product Image"
-                        />
-                      </div>
-                      <div class="card-body col col-lg-10">
-                        <div class="card-info">
-                          <div class="card-title">
-                            <h1 class="ts24b">
-                              Nome Completo do Produto Nome Completo do Produto
-                            </h1>
-                            <h2 class="ts14b ts-purple">
-                              CÓDIGO DO PRODUTO: XXXXXX
-                            </h2>
-                          </div>
-                          <div class="card-text-cart">
-                            <div class="product-amount">
-                              <div class="counter">
-                                <button
-                                  class="btn btn-purple ts24r"
-                                  onclick="this.blur();"
-                                >
-                                  -
-                                </button>
-                                <div class="counter-display ts24r ts-black">
-                                  01
+                            <div class="card-text-cart">
+                              <div class="product-amount">
+                                <div class="counter">
+                                  <button
+                                    class="btn btn-purple ts24r"
+                                    onclick="this.blur();"
+                                  >
+                                    -
+                                  </button>
+                                  <div class="counter-display ts24r ts-black">
+                                    {{ p.name }}
+                                  </div>
+                                  <button
+                                    class="btn btn-purple ts24r"
+                                    onclick="this.blur();"
+                                  >
+                                    +
+                                  </button>
                                 </div>
-                                <button
-                                  class="btn btn-purple ts24r"
-                                  onclick="this.blur();"
-                                >
-                                  +
-                                </button>
                               </div>
+                              <h3 class="card-text ts28b">R$ 100,00</h3>
                             </div>
-                            <h3 class="card-text ts28b">R$ 100,00</h3>
+                          </div>
+                          <div class="card-icon">
+                            <a href="">
+                              <img
+                                src="../../../assets/img/icon/trash-fill-purple.svg"
+                                class="icon-32"
+                              />
+                            </a>
                           </div>
                         </div>
-                        <div class="card-icon">
-                          <a href="">
-                            <img
-                              src="../../../assets/img/icon/trash-fill-purple.svg"
-                              class="icon-32"
-                            />
-                          </a>
-                        </div>
                       </div>
-                    </div>
+                    </ul>
                   </a>
-                </ul>
+                </div>
               </li>
               <section class="purchase-additional-info">
                 <form id="coupon-form" method="post" action="" class="ts14r">
@@ -186,7 +137,9 @@
                   <button class="btn btn-green btn-buy ts24b" type="submit">
                     Concluir a compra
                   </button>
-                  <a class="ts14b link-green" href="">Limpar carrinho</a>
+                  <a v-on:click="deleteCart()" class="ts14b link-green" href=""
+                    >Limpar carrinho</a
+                  >
                 </div>
               </div>
             </div>
@@ -205,17 +158,29 @@ import CartEmpty from "./CartEmpty.vue";
 export default {
   data() {
     return {
-      products: [],
+      cartItems: null,
+      products: null,
       resultAmount: 0,
     };
   },
   methods: {
-    async getProductById() {
+    async getProductById(id, amount) {
       var resposta = await fetch(
-        `https://localhost:7016/api/v1/Product/details?id=${this.id}`
+        `https://localhost:7016/api/v1/Product/details?id=${id}`
+      );
+      console.log(
+        `Request https://localhost:7016/api/v1/Product/details?id=${id}`
       );
       var json = await resposta.json();
-      this.product = json;
+      //return json;
+      var product = {
+        id: json.id,
+        name: json.name,
+        price: json.price,
+        discount: json.discount,
+        amount: amount,
+      };
+      return product;
     },
     counterAdd() {
       this.counter++;
@@ -226,33 +191,41 @@ export default {
     getCart() {
       if (localStorage.getItem("cart")) {
         this.cartItems = JSON.parse(localStorage.getItem("cart"));
+        console.log(this.cartItems);
       }
-      this.resultAmount = this.cartItems.length();
+      this.resultAmount = this.cartItems.length;
+      console.log("GET CART!! cart items", this.cartItems);
+      this.getProducts();
     },
     addToCart() {
-      let cartItems = [];
-      if (localStorage.getItem("cart")) {
-        cartItems = JSON.parse(localStorage.getItem("cart"));
-      }
-
-      if (cartItems.find((item) => item.productId == this.product.id)) {
-        let index = cartItems.findIndex(
+      if (this.cartItems.find((item) => item.productId == this.product.id)) {
+        let index = this.cartItems.findIndex(
           (item) => item.productId == this.product.id
         );
-        cartItems[index].amount += this.counter;
-      } else {
-        cartItems.push({
-          productId: this.product.id,
-          amount: this.counter,
-        });
+        this.cartItems[index].amount = this.counter;
       }
-      localStorage.setItem("cart", JSON.stringify(cartItems));
-      console.log(localStorage.getItem("cart"));
-      this.$router.push(`/carrinho`);
+      localStorage.setItem("cart", JSON.stringify(this.cartItems));
+      console.log("cart items", this.cartItems);
+      this.getCart();
+    },
+    deleteCart() {
+      localStorage.removeItem("cart", JSON.stringify(this.cartItems));
+    },
+    getProducts() {
+      this.products = [];
+
+      for (let p in this.cartItems) {
+        this.products.push(this.getProductById(p.productId, p.amount));
+        console.log("cart items", p.productId);
+      }
+      console.log("GET PRODUCTS!! cart items", this.products);
+    },
+    showProductDetail(productId) {
+      this.$router.push(`/produto/${productId}`);
     },
   },
   beforeMount() {
-    this.getProductById();
+    this.getCart();
   },
   components: {
     CartEmpty: CartEmpty,
