2aece7fcbef6ced96c6fc94c25cd9dd8f259d781
Maíra Heloísa Silva Oliveira <heloisa3031@gmail.com>
2022-08-27 12:05:29 -0300

    Address API comunication

--- src/components/User/Address/AddressList.vue
+++ src/components/User/Address/AddressList.vue
@@ -7,14 +7,18 @@
             <section>
                 <p class="ts12r ts-green">Mantenha seus endereços sempre atualizados e identificados para facilitar sua compra.</p>
             </section>
+            <div v-if="resultAmount">
             <section>
                 <div class="result-count">
-                    <p class="ts18b">XXX endereços adicionados</p>
+                    <p class="ts18b">{{ resultAmount }} endereços adicionados</p>
                 </div>
             </section>
-            <li class="cards-list">
-                <ul>
-                    <a href="">
+            <div
+            v-for="a in addresses"
+            :key="a.id"
+          >
+            <div class="cards-list">
+                    <a v-on:click="showAddressDetail(a.id)">
                         <div class="card card-item">
                             <div class="col col-lg-2">
                                 <img class="card-img-h" src="../../../assets/img/icon/house-door-fill.svg" alt="House Image">
@@ -22,54 +26,74 @@
                             <div class="card-body-data col col-lg-10">
                                 <div class="card-info">
                                     <div class="card-title">
-                                        <h1 class="ts18b ts-green">Casa</h1>
-                                        <p class="ts14r ts-green">Endereço, Número - Complemento</p>
-                                        <p class="ts14r ts-green">Ponto de referência</p>
-                                        <p class="ts14r ts-green">Bairro - Cidade - UF</p>
-                                        <p class="ts14r ts-green">CEP</p>
+                                        <h1 class="ts18b ts-green">{{ a.destinatary }}</h1>
+                                        <p class="ts14r ts-green">{{ a.address }}, {{ a.addressNumber }} - {{ a.complement }}</p>
+                                        <p class="ts14r ts-green">{{ a.reference }}</p>
+                                        <p class="ts14r ts-green">{{ a.neighborhood }} - {{ a.city }} - {{ a.state }}</p>
+                                        <p class="ts14r ts-green">{{ a.cep }}</p>
                                     </div>
                                     <div>
+                                        <RouterLink to="/minha-conta/enderecos/editar">
                                         <nav>
                                             <a class="ts14b link-green" href="">Editar</a>
                                         </nav>
+                                        </RouterLink>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </a>
-                </ul>
-                <ul>
-                    <a href="">
-                        <div class="card card-item">
-                            <div class="col col-lg-2">
-                                <img class="card-img-h" src="../../../assets/img/icon/house-door-fill.svg" alt="Building Icon">
-                            </div>
-                            <div class="card-body-data col col-lg-10">
-                                <div class="card-info">
-                                    <div class="card-title">
-                                        <h1 class="ts18b ts-green">Trabalho</h1>
-                                        <p class="ts14r ts-green">Endereço, Número - Complemento</p>
-                                        <p class="ts14r ts-green">Ponto de referência</p>
-                                        <p class="ts14r ts-green">Bairro - Cidade - UF</p>
-                                        <p class="ts14r ts-green">CEP</p>
-                                    </div>
-                                    <div>
-                                        <nav>
-                                            <a class="ts14b link-green" href="">Editar</a>
-                                        </nav>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </ul>
-            </li>
+            </div>
+            </div>
+            </div>
+            <div v-else>
+                <AddressEmpty></AddressEmpty>
+            </div>
             <div class="col-md-8 col-lg-3" id="center-btn">
-                <a href="">
-                    <button class="btn btn-purple ts18b" type="submit">Adicionar endereço</button>
-                </a>
+                 <a href="#" class="row">
+                <RouterLink to="minha-conta/enderecos/adicionar">
+                <button class="btn btn-purple btn-48 ts18b" type="submit">Adicionar endereço</button>
+            </RouterLink>
+            </a>
+            
             </div>
         </article>
     </main>
 </template>
-<script></script>
+<script>
+    import AddressEmpty from "./AddressEmpty.vue";
+    export default {
+  data() {
+    return {
+    userId: localStorage.getItem("userId"),
+    token: localStorage.getItem("token"),
+      addresses: [],
+      currentPage: 0,
+      resultAmount: 0,
+    };
+  },
+  methods: {
+    async getAddresses() {
+      var resposta = await fetch(
+        `https://localhost:7016/api/v1/Address?UserId=${this.userId}`, {headers: {
+    'Authorization': `bearer ${this.token}`,
+  },
+});
+      var json = await resposta.json();
+      this.addresses = json.response;
+      this.currentPage = json.pageNumber;
+      this.resultAmount = json.previousCount + json.amount + json.nextCount;
+    },
+    showAddressDetail(addressId) {
+      this.$router.push(`/minha-conta/enderecos/editar/${addressId}`);
+    },
+  },
+  beforeMount() {
+    this.currentPage = 0;
+    this.getAddresses();
+  },
+  components: {
+    AddressEmpty: AddressEmpty,
+  },
+};
+</script>
