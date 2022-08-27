a8c500f0bfb1567da026ea08b966fcef67c3cd91
Maíra Heloísa Silva Oliveira <heloisa3031@gmail.com>
2022-08-27 12:04:36 -0300

    Address API comunication

--- src/components/User/Address/AddressEmpty.vue
+++ src/components/User/Address/AddressEmpty.vue
@@ -1,12 +1,6 @@
 <template>
   <main>
         <article class="account">
-        <section>
-            <h1 class="ts40 ts-purple">Meus endereços</h1>
-        </section>
-        <section>
-            <p class="ts12r ts-green">Mantenha seus endereços sempre atualizados e identificados para facilitar sua compra.</p>
-        </section>
         <section class="customer-empty-container">
             <div class="card card-empty col col-lg-10">
                 <img src="../../../assets/img/icon/geo-alt-fill.svg" class="icon-64" alt="Map icon">
@@ -14,10 +8,6 @@
                 <h2 class="ts14r col-lg-6">Você ainda não adicionou nenhum endereço.
                     Adicione um novo endereço no botão abaixo.</h2>
             </div>
-
-            <a href="#" class="row">
-                <button class="btn btn-purple btn-48 ts18b" type="submit">Adicionar endereço</button>
-            </a>
         </section>
     </article>
     </main>
